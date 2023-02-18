5141faa2b4d5fb00ec7965af55f02a02ab2f6592
Anusree-6 <124380542+Anusree-6@users.noreply.github.com>
2023-02-18 18:59:59 +0530

    Add files via upload

---
+++ case study 02 (problem 7)-input.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 18 17:40:05 2023
+
+@author: anusree
+"""
+
+#to remove None values from a given list using the lambda function.
+
+def remove_none(nums):
+    result = filter(lambda v: v is not None, nums)
+    return list(result)
+
+nums = [12, 0, None, 23, None, -55, 234, 89, None, 0, 6, -12]
+print("Original list:")
+print(nums)
+print("\nRemove None value from the said list:")
+print(remove_none(nums))
