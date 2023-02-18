71494038cad3d931dbebee182ed80ad1ae44e43f
Anusree-6 <124380542+Anusree-6@users.noreply.github.com>
2023-02-18 19:06:43 +0530

    Add files via upload

---
+++ case study 02 (problem 9)-input.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 18 18:03:08 2023
+
+@author: anusree
+"""
+
+class BankAccount:
+    # create the constuctor with parameters: accountNumber, name and balance 
+    def __init__(self,accountNumber, name, balance):
+        self.accountNumber = accountNumber
+        self.name = name
+        self.balance = balance
+        
+    # create Deposit() method
+    def Deposit(self , d ):
+        self.balance = self.balance + d
+    
+    # create Withdrawal method
+    def Withdrawal(self , w):
+        if(self.balance < w):
+            print("impossible operation! Insufficient balance !")
+        else:
+            self.balance = self.balance - w
+    # create bankFees() method
+    def bankFees(self):
+        self.balance = (95/100)*self.balance
+        
+    # create display() method
+    def display(self):
+        print("Account Number : " , self.accountNumber)
+        print("Account Name : " , self.name)
+        print("Account Balance : " , self.balance , "$")
+        
+# Testing the code :
+newAccount = BankAccount(346788909, "Anusree" , 5500)
+# Creating Withdrawal Test
+newAccount.Withdrawal(300)
+# Create deposit test
+newAccount.Deposit(200)
+# Display account informations
+newAccount.display()
